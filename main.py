8831ac16d1c406f0c1d102429e48ce8e7471e7be
DarioRM <drosas@e-bitware.com.mx>
2022-07-07 14:37:00 -0500

    tweets model created

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 # Pydantic
@@ -13,16 +13,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 # Models
 class UserBase(BaseModel):
     userID: UUID = Field(...)
     email: EmailStr = Field(...)
+
 
 class UserLogin(UserBase):
     password: str = Field(
         ...,
         min_length=8
     )
+
 
 class User(UserBase):
     first_name: str = Field(
@@ -37,8 +40,17 @@ class User(UserBase):
     )
     birth_date: Optional[date] = Field(default=None)
 
+
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 
 @app.get("/")
